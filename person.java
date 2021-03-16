0f90585cbecbcb9b961045aa155685e32870e8a1
ingefatul <ingefatulkomariah@gmail.com>
2021-03-16 06:08:11 -0700

    Create person.java

---
+++ person.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugaswdsi;
+
+/**
+ *
+ * @author ingee
+ */
+public class person {
+    public static void main(String[] args) {
+
+ String fName, lName, stuId, stuStatus;
+ fName = "Siti Ingefatul";
+ lName = "Qomariah";                                         
+ stuId = "E41200122";
+ stuStatus = "Active";
+ 
+ System.out.println("Student Name\t: " + fName + " " + lName);
+ System.out.println("Student ID  \t: " + stuId);
+ System.out.println("Status      \t: " + stuStatus);
+    }
+}
