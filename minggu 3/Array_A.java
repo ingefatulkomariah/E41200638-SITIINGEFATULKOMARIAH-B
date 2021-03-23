cbfe3b23fc3862684514366b1dc469c21069c849
ingefatul <ingefatulkomariah@gmail.com>
2021-03-23 00:54:22 -0700

    Create Array_A.java

---
+++ minggu 3/Array_A.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas.array.minggu.pkg3;
+
+/**
+ *
+ * @author ingee
+ */
+public class TugasArrayMinggu3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String[] nama = {"Siti","ingefatul","komariah"};
+        
+        for(int i=0; i<nama.length; i++) {
+            System.out.print(nama[i]);
+            System.out.print(" ");
+        }
+    }
+    
+}
