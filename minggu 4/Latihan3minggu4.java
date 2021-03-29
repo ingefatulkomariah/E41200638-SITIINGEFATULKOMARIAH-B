f323b2e4bde5b8554ea6bb7d66fd753161de992c
ingefatul <ingefatulkomariah@gmail.com>
2021-03-29 07:46:46 -0700

    Create Latihan3minggu4.java

---
+++ minggu 4/Latihan3minggu4.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan3minggu4;
+
+/**
+ *
+ * @author ingee
+ */
+public class Latihan3minggu4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 0; j<= i; j++) {
+                System.out.print("*");
+                
+            }
+            System.out.println("");
+        }
+    }
+    
+}
