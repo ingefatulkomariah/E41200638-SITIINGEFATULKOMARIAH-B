815307a7432366031555561f0628a5a8ed483c78
ingefatul <ingefatulkomariah@gmail.com>
2021-03-30 03:43:11 -0700

    Create Tugas5B.java

---
+++ minggu 4/Tugas5B.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas5b;
+
+/**
+ *
+ * @author ingee
+ */
+public class Tugas5B {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("        Do_while");
+        System.out.println("Bilangan kelipatan 2 (1-100)");
+        System.out.println("============================");
+        
+        int i = 1;
+        do {
+            System.out.print(i + " ");
+            i *= 2;
+        } 
+        while ( i < 100);
+    }
+}
+   
