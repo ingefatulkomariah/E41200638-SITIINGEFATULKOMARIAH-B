9ee785f15e6b711ab2c6180015469903dcf84ef4
ingefatul <ingefatulkomariah@gmail.com>
2021-04-25 13:20:00 -0700

    Update Animal.java

--- minggu 7/Animal.java
+++ minggu 7/Animal.java
@@ -11,7 +11,25 @@ package tugasw7;
  */
 public class Animal {
     public void makeNoise () {
-        System.out.println("talk");
-        
+        System.out.println("talk");   
     }
 }
+class Dog extends Animal{
+    @Override
+    public void makeNoise () {
+        System.out.println("bark");  
+    }
+    public static void main (String[] args) {
+    Animal animal = new Animal () ;
+        animal.makeNoise ();
+        Dog dog = new Dog();
+        dog.makeNoise () ;
+        
+        if (animal instanceof Animal)
+        System.out.println("animal is Animal");
+        if (dog instanceof Animal)
+        System.out.println("dog is Animal");
+        if (animal instanceof Dog)
+        System.out.println("animal is Dog");
+    } 
+        }
