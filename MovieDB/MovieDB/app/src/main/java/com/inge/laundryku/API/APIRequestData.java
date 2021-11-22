f4c4bb02a21a7875beb56fe97393f61bae429219
ingefatul <ingefatulkomariah@gmail.com>
2021-11-22 14:04:38 -0800

    movieDB

---
+++ MovieDB/MovieDB/app/src/main/java/com/inge/laundryku/API/APIRequestData.java
@@ -0,0 +1,13 @@
+package com.inge.laundryku.API;
+
+import com.rtegar.laundryku.Model.ResponseModel;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface APIRequestData {
+
+    @GET("retrieve.php")
+    Call<ResponseModel> ardRetrieveData();
+
+}
